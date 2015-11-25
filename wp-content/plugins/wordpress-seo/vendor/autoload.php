5fdf9b8d7b658604192ce46ce920227a863a0e40
delarasj <delarasj@wibidei.com>
2015-11-26 00:21:45 +0100

    Se actualiz el Wordpress SEO

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0b89a5a2c2b62f1e96b89adedb1dc11f::getLoader();
+return ComposerAutoloaderInit4ebb370bed06ea1715ee8069dc47de2d::getLoader();
